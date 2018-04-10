434807eec58855f6cdcdad3471a6f8ed79808803
Francisco Benavides <francisco.benavides@gmail.com>
2018-04-10 13:42:01 -0500

    Added robots.txt and sitemap.xml support

--- application/urls.py
+++ application/urls.py
@@ -5,6 +5,8 @@
 
 from flask import Blueprint
 
+from .views import sitemap
+from .views import static_from_root
 from .views import favicon
 from .views import index
 from .views import inside
@@ -12,6 +14,8 @@ from .views import inside
 
 mod = Blueprint('main', __name__)
 
+mod.add_url_rule('/sitemap.xml', view_func=sitemap)
+mod.add_url_rule('/robots.txt', view_func=static_from_root)
 mod.add_url_rule('/favicon.ico', view_func=favicon)
 mod.add_url_rule('/', view_func=index)
 mod.add_url_rule('/inside', view_func=inside)
--- application/views.py
+++ application/views.py
@@ -2,10 +2,47 @@
 ''' Views
 
     Define the routing views'''
-
+from datetime import datetime
+from datetime import timedelta
+from flask import url_for
+from flask import request
 from flask import redirect
+from flask import make_response
 from flask import render_template
+from flask import send_from_directory
+from flask import current_app
 from flask_security import login_required
+
+from .models import User
+
+
+def sitemap():
+    ''' Generate sitemap.xml, makes a list of urls and modified date'''
+
+    pages = []
+    ten_days_ago = datetime.now() - timedelta(days=10)
+
+    # static pages
+    for rule in current_app.url_map.iter_rules():
+        if 'GET' in rule.methods and len(rule.arguments) == 0:
+            pages.append([rule.rule, ten_days_ago.isoformat()])
+
+    # user model pages
+    users = User.query.order_by(User.updated_at).all()
+    for user in users:
+        url = url_for('main.index', name=user.username)
+        modified_time = user.updated_at.date().isoformat()
+        pages.append([url, modified_time])
+
+    sitemap_xml = render_template('sitemap.xml', pages=pages)
+    response = make_response(sitemap_xml)
+    response.headers["Content-Type"] = "application/xml"
+
+    return response
+
+
+def static_from_root():
+    return send_from_directory(current_app.static_folder, request.path[1:])
 
 
 def favicon():
