fc9b194a4bd2ec42e9610dae5738c79d499722fe
Francisco Benavides <francisco.benavides@gmail.com>
2018-04-13 15:11:38 -0500

    Change from native flask server to tornado non-blocking server for added speed and features!

--- run.py
+++ run.py
@@ -1,9 +1,27 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+from tornado.web import FallbackHandler, RequestHandler, Application
 from application import create_app
 
-if __name__ == '__main__':
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.write("This message comes from Tornado ^_^")
+
+
+tr = WSGIContainer(create_app())
+
+
+tornado = Application([
+    (r"/tornado", MainHandler),
+    (r".*", FallbackHandler, dict(fallback=tr)),
+])
+
+
+if __name__ == "__main__":
     ''' Run the application'''
-    app = create_app()
-    app.run('0.0.0.0')
+    tornado.listen(5000, address='0.0.0.0')
+    IOLoop.instance().start()
