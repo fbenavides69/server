56b547d5f0069fca807ba3dde4a79946cfb72a14
Francisco Benavides <francisco.benavides@gmail.com>
2018-04-24 18:33:07 -0500

    Updated GraphQL schema fileds to view

--- application/graphql/schema.py
+++ application/graphql/schema.py
@@ -10,11 +10,14 @@ from graphene_sqlalchemy import SQLAlchemyConnectionField
 from application.models import User as ModelUser
 from application.models import Role as ModelRole
 
+from flask import g
+
 
 class User(SQLAlchemyObjectType):
     class Meta:
         model = ModelUser
         interfaces = (relay.Node, )
+        exclude_fields = ['password']
 
 
 class Role(SQLAlchemyObjectType):
@@ -25,8 +28,16 @@ class Role(SQLAlchemyObjectType):
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
+    viewer = graphene.Field(User, )
     all_users = SQLAlchemyConnectionField(User)
     all_roles = SQLAlchemyConnectionField(Role)
+
+    def resolve_viewer(self, args, context, info):
+        try:
+            logged_in_user = g.user
+        except AttributeError:
+            return None
+        return logged_in_user
 
 
 schema = graphene.Schema(query=Query)
